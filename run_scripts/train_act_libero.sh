2be15ccc8ebd50b47139e7f9d93f6f5ba968cfb8
Jesse Zhang <jessezhang5@gmail.com>
2025-05-29 17:36:39 -0700

    update train act libero.sh train act libero path

---
+++ run_scripts/train_act_libero.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --account=biyik_1165
+#SBATCH --nodes=1
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=64G
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --output=slurm_outputs/%x_%j.out
+#SBATCH --error=slurm_outputs/%x_%j.err
+
+if [ $RELAUNCH_COUNT -ge $MAX_RELAUNCHES ]; then
+  echo "Maximum relaunch limit ($MAX_RELAUNCHES) reached. Exiting."
+  exit 1
+fi
+
+echo "Starting job... Relaunch attempt: $((RELAUNCH_COUNT + 1))/$MAX_RELAUNCHES"
+source ~/.bashrc
+module load cuda
+module load glew
+module load patchelf
+module load git-lfs
+export PATH="/apps/conda/.local/bin:$PATH"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
+export MUJOCO_GL=egl
+
+EXP_NAME=train_act_libero
+PROJ_NAME=lerobot
+
+BASE_TRAIN_CMD="python lerobot/scripts/train.py --config_path train_configs/train_act_libero.yaml --wandb.run_id=$EXP_NAME"
+
+#TRAIN_CMD="$BASE_TRAIN_CMD --overwrite"
+TRAIN_CMD="$BASE_TRAIN_CMD"
+#TRAIN_CMD="$BASE_TRAIN_CMD --resume"
+echo "Executing command: $TRAIN_CMD"
+$TRAIN_CMD
