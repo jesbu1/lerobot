90253060ae5e3ef20c3980f63fc829d4a5c70d2c
Jesse Zhang <jessezhang5@gmail.com>
2025-06-10 23:02:09 -0700

    resize and pad and convert to uint8

--- lerobot/common/envs/wrappers.py
+++ lerobot/common/envs/wrappers.py
@@ -11,6 +11,7 @@ from gymnasium import spaces
 
 import re
 import time
+from PIL import Image
 import base64
 import cv2
 import numpy as np
@@ -27,6 +28,63 @@ DOWNSAMPLE_RESOLUTION = 256
 # PATH_MODEL_NAME_MASK = "vila_3b_oxe_no_droid_path_mask"
 PATH_MODEL_NAME = "vila_3b_oxe_sim_path"
 PATH_MODEL_NAME_MASK = "vila_3b_oxe_sim_path_mask"
+
+def convert_to_uint8(img: np.ndarray) -> np.ndarray:
+    """Converts an image to uint8 if it is a float image.
+
+    This is important for reducing the size of the image when sending it over the network.
+    """
+    if np.issubdtype(img.dtype, np.floating):
+        img = (255 * img).astype(np.uint8)
+    return img
+
+
+def _resize_with_pad_pil(image: Image.Image, height: int, width: int, method: int) -> Image.Image:
+    """Replicates tf.image.resize_with_pad for one image using PIL. Resizes an image to a target height and
+    width without distortion by padding with zeros.
+
+    Unlike the jax version, note that PIL uses [width, height, channel] ordering instead of [batch, h, w, c].
+    """
+    cur_width, cur_height = image.size
+    if cur_width == width and cur_height == height:
+        return image  # No need to resize if the image is already the correct size.
+
+    ratio = max(cur_width / width, cur_height / height)
+    resized_height = int(cur_height / ratio)
+    resized_width = int(cur_width / ratio)
+    resized_image = image.resize((resized_width, resized_height), resample=method)
+
+    zero_image = Image.new(resized_image.mode, (width, height), 0)
+    pad_height = max(0, int((height - resized_height) / 2))
+    pad_width = max(0, int((width - resized_width) / 2))
+    zero_image.paste(resized_image, (pad_width, pad_height))
+    assert zero_image.size == (width, height)
+    return zero_image
+
+
+def resize_with_pad(images: np.ndarray, height: int, width: int, method=Image.BILINEAR) -> np.ndarray:
+    """Replicates tf.image.resize_with_pad for multiple images using PIL. Resizes a batch of images to a target height.
+
+    Args:
+        images: A batch of images in [..., height, width, channel] format.
+        height: The target height of the image.
+        width: The target width of the image.
+        method: The interpolation method to use. Default is bilinear.
+
+    Returns:
+        The resized images in [..., height, width, channel].
+    """
+    # If the images are already the correct size, return them as is.
+    if images.shape[-3:-1] == (height, width):
+        return images
+
+    original_shape = images.shape
+
+    images = images.reshape(-1, *original_shape[-3:])
+    resized = np.stack(
+        [_resize_with_pad_pil(Image.fromarray(im), height, width, method=method) for im in images]
+    )
+    return resized.reshape(*original_shape[:-3], *resized.shape[-3:])
 
 
 def draw_onto_image(vlm_path_mask_output, prompt_type, img, verbose=False):
@@ -411,8 +469,12 @@ class LIBEROEnv(gym.Env):
         new_obs = {}
         pixels = {}
         # following stupid lerobot hardcoded pixels naming...
-        pixels["agentview_image"] = flipped_agentview
-        pixels["robot0_eye_in_hand_image"] = flipped_eye_in_hand
+        pixels["agentview_image"] = convert_to_uint8(
+            resize_with_pad(flipped_agentview, self.LIBERO_ENV_RESOLUTION, self.LIBERO_ENV_RESOLUTION)
+        )
+        pixels["robot0_eye_in_hand_image"] = convert_to_uint8(
+            resize_with_pad(flipped_eye_in_hand, self.LIBERO_ENV_RESOLUTION, self.LIBERO_ENV_RESOLUTION)
+        )
         new_obs["pixels"] = pixels
         # following stupid lerobot hardcoded agent_pos naming...
         new_obs["agent_pos"] = np.concatenate(
