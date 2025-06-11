0d5cced36a44d103f5dc7ee13436a8d9b5ad6927
Jesse Zhang <jessezhang5@gmail.com>
2025-06-10 23:19:26 -0700

    resize and pad and convert to uint8

--- lerobot/common/envs/configs.py
+++ lerobot/common/envs/configs.py
@@ -176,8 +176,8 @@ class LIBEROEnv(EnvConfig):
         default_factory=lambda: {
             "action": ACTION,
             "agent_pos": OBS_ROBOT,  # agent_pos is required name for lerobot
-            "pixels/agentview_image": f"{OBS_IMAGES}.agentview_image",
-            "pixels/robot0_eye_in_hand_image": f"{OBS_IMAGES}.robot0_eye_in_hand_image",
+            "pixels/agentview_image": "image",
+            "pixels/robot0_eye_in_hand_image": "wrist_image",
         }
     )
 
