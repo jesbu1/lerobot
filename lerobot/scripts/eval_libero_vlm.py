7e4dd9892c3adbea77316d38d2bcaa1fcc2ee59e
Jesse Zhang <jessezhang5@gmail.com>
2025-07-02 08:50:39 -0700

    remove assert

--- lerobot/scripts/eval_libero_vlm.py
+++ lerobot/scripts/eval_libero_vlm.py
@@ -141,7 +141,7 @@ def make_libero_env(
 @parser.wrap()
 def eval_main(cfg: EvalPipelineConfig):
     logging.info(pformat(asdict(cfg)))
-    assert cfg.eval.n_episodes == 50, "n_episodes must be 50 for libero"
+    # assert cfg.eval.n_episodes == 50, "n_episodes must be 50 for libero"
 
     # Check device is available
     device = get_safe_torch_device(cfg.policy.device, log=True)
