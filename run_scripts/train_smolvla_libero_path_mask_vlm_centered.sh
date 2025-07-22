c44441802ec5beacdebd551ead1114300748a010
Jesse Zhang <jessezhang5@gmail.com>
2025-07-21 17:21:50 -0700

    adding new smolvla path vlm training scripts

--- run_scripts/train_smolvla_libero_path_mask_vlm_centered.sh
+++ run_scripts/train_smolvla_libero_path_mask_vlm_centered.sh
@@ -28,10 +28,10 @@ export HF_HUB_DISABLE_PROGRESS_BARS=0
 
 RESUME=false
 
-EXP_NAME=train_smolvla_libero_path_mask_vlm_centered_0.1mask_nowrist
+EXP_NAME=train_smolvla_libero_path_mask_vlm_centered_0.01-0.12mask
 PROJ_NAME=lerobot
 
-BASE_TRAIN_CMD="--config_path=train_configs/train_smolvla_libero_path_mask_vlm_centered_nowrist.yaml --wandb.run_id=$EXP_NAME --output_dir=outputs/$EXP_NAME --job_name=$EXP_NAME"
+BASE_TRAIN_CMD="--config_path=train_configs/train_smolvla_libero_path_mask_vlm_centered.yaml --wandb.run_id=$EXP_NAME --output_dir=outputs/$EXP_NAME --job_name=$EXP_NAME"
 RESUME_TRAIN_CMD="--config_path=outputs/$EXP_NAME/checkpoints/last/pretrained_model/train_config.json --resume=true"
 
 TRAIN_CMD="conda run -n lerobot --no-capture-output python lerobot/scripts/train.py"
