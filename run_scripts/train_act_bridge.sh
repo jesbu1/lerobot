0261420a59e66ef713bd21cc2fb03d98fd072e19
Jesse Zhang <jessezhang5@gmail.com>
2025-07-28 11:10:13 -0700

    train act bridge .sh

--- run_scripts/train_act_bridge.sh
+++ run_scripts/train_act_bridge.sh
@@ -18,9 +18,9 @@ module load git-lfs
 export PATH="/apps/conda/.local/bin:$PATH"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
 export MUJOCO_GL=egl
-RESUME=true
+RESUME=false
 
-EXP_NAME=train_act_bridge_2cam_lang
+EXP_NAME=train_act_bridge_1cam
 PROJ_NAME=lerobot
 
 BASE_TRAIN_CMD="--config_path=train_configs/train_act_bridge.yaml --output_dir=outputs/$EXP_NAME --job_name=$EXP_NAME --wandb.run_id=$EXP_NAME"
--- run_scripts/train_smolvla_bridge.sh
+++ run_scripts/train_smolvla_bridge.sh
@@ -20,7 +20,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia
 export MUJOCO_GL=egl
 RESUME=false
 
-EXP_NAME=train_smolvla_bridge_2cam
+EXP_NAME=train_smolvla_bridge_1cam
 PROJ_NAME=lerobot
 
 BASE_TRAIN_CMD="--config_path=train_configs/train_smolvla_bridge.yaml --output_dir=outputs/$EXP_NAME --job_name=$EXP_NAME --wandb.run_id=$EXP_NAME"
