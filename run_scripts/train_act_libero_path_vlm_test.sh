e123ab4c99300c1326eb818bd124bbf3819ca26a
Jesse Zhang <jessezhang5@gmail.com>
2025-08-01 15:17:55 -0700

    update with new configs and everything for new exps

--- run_scripts/train_act_libero_path_vlm_test.sh
+++ run_scripts/train_act_libero_path_vlm_test.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=biyik_1165
 #SBATCH --nodes=1
 #SBATCH --time=48:00:00
-#SBATCH --cpus-per-task=24
+#SBATCH --cpus-per-task=20
 #SBATCH --mem=32G
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
@@ -10,12 +10,12 @@
 #SBATCH --error=slurm_outputs/%x_%j.err
 
 source ~/.bashrc
+
+EXP_NAME=train_act_libero_path_vlm_test_5ep
+PROJ_NAME=lerobot
 RESUME=false
 
-EXP_NAME=train_smolvla_libero_path_mask_vlm_new
-PROJ_NAME=lerobot
-
-BASE_TRAIN_CMD="--config_path=train_configs/train_smolvla_libero_path_mask_vlm.yaml --wandb.run_id=$EXP_NAME --output_dir=outputs/$EXP_NAME --job_name=$EXP_NAME"
+BASE_TRAIN_CMD="--config_path=train_configs/train_act_libero_path_vlm.yaml --output_dir=outputs/$EXP_NAME --job_name=$EXP_NAME --wandb.run_id=$EXP_NAME --dataset.repo_id=jesbu1/libero_test_lerobot_pathmask_vlm_preds_max_ep_per_task_5"
 RESUME_TRAIN_CMD="--config_path=outputs/$EXP_NAME/checkpoints/last/pretrained_model/train_config.json --resume=true"
 
 TRAIN_CMD="conda run -n lerobot --no-capture-output python lerobot/scripts/train.py"
@@ -25,4 +25,4 @@ else
     TRAIN_CMD="$TRAIN_CMD $BASE_TRAIN_CMD"
 fi
 echo "Executing command: $TRAIN_CMD"
-$TRAIN_CMD 
+$TRAIN_CMD
