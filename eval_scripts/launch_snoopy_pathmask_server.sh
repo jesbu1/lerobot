22534fbc4b83e3b5c0cc7428e09b7769ccbcfbed
jesse zhang <jessez@usc.edu>
2025-09-03 19:25:36 +0000

    diffusion new

--- eval_scripts/launch_snoopy_pathmask_server.sh
+++ eval_scripts/launch_snoopy_pathmask_server.sh
@@ -10,10 +10,10 @@
 
 policy_port=8001
 #checkpoint=outputs/train_smolvla_bridge_pathmask_imgtransforms/checkpoints/last/pretrained_model
-checkpoint=outputs/train_diffusion_bridge_pathmask/checkpoints/last/pretrained_model
-#checkpoint=outputs/train_diffusion_bridge/checkpoints/last/pretrained_model/
+#checkpoint=outputs/train_diffusion_bridge_pathmask/checkpoints/last/pretrained_model
+checkpoint=outputs/train_diffusion_bridge/checkpoints/last/pretrained_model/
 #checkpoint=outputs/train_smolvla_bridge_imgtransforms/checkpoints/last/pretrained_model/
-serve_policy_vlm_freq=5
+serve_policy_vlm_freq=10
 
 if [[ "$checkpoint" == *"path"* ]]; then
     cd ~/VILA
